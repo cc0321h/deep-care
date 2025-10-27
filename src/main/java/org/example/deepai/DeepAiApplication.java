e6f01c51003b47c9f9176238c37aa21c7b0266c8
cc0321h <1519080933@qq.com>
2025-10-27 10:57:30 +0800

    test

---
+++ src/main/java/org/example/deepai/DeepAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.deepai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/deepai/DeepAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deepai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
